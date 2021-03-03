5f48372789b90777fb35cb702e782f070826a668
Ruslans Babajans <ruslans.babajans@mail.ru>
2021-03-03 13:10:03 +0200

    Started LD2

---
+++ LD/LD2/LD2.m
@@ -0,0 +1,24 @@
+%% Clear variables and close figures
+format long
+clear variables
+close all
+
+%==============================================%
+%% Parameters
+
+Fs=100e6; % baseband clock
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
